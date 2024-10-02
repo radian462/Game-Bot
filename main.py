9342f0d97bfb64c24714a02f970431ebf3bd5e49
radian462 <102207047+radian462@users.noreply.github.com>
2024-10-03 08:39:05 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+import os
+from keep_alive import keep_alive
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print('ログインしました')
+
+keep_alive()
+client.run(os.getenv("DISCORD_TOKEN"))
