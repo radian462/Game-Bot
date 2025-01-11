871681a08bdd781de83fbe06ae418b761c1093ca
radian462 <102207047+radian462@users.noreply.github.com>
2025-01-11 11:47:29 +0000

    main.pyにコマンドを追加し、gitignoreを追加。

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
 
 import discord
-
-from keep_alive import keep_alive
-
+from discord import app_commands
 
 client = discord.Client(intents=discord.Intents.default())
+tree = app_commands.CommandTree(client)
 
 
 @client.event
 async def on_ready():
     print("ログインしました")
+
+
+@tree.command(name="werewolf", description="人狼ゲームをプレイします")
+async def werewolf(interaction: discord.Interaction):
+    await interaction.response.defer()
+    embed = discord.Embed(title="人狼")
+    await interaction.followup.send(embed=embed)
 
 
 client.run(os.getenv("DISCORD_TOKEN"))
