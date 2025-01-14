d689a2ffce249b0f217845f32c783eed50f16c36
radian462 <102207047+radian462@users.noreply.github.com>
2025-01-14 11:54:32 +0000

    リファクタリングを実施

--- main.py
+++ main.py
@@ -15,7 +15,123 @@ async def on_ready():
     print("ログインしました")
 
 
-def make_participants_embed(game_info: dict) -> discord.Embed:
+class JoinView(discord.ui.View):
+    def __init__(self, id: str, timeout: int | None = None):
+        super().__init__(timeout=timeout)
+        self.id = id
+
+    @discord.ui.button(label="参加", style=discord.ButtonStyle.success)
+    async def join(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user.id not in werewolf_manager.games[self.id]["players"]:
+            if (
+                len(werewolf_manager.games[self.id]["players"]) + 1
+                >= werewolf_manager.games[self.id]["limit"]
+            ):
+                await interaction.response.send_message(
+                    "人数制限に達しました", ephemeral=True
+                )
+                return
+
+            if interaction.user.id != werewolf_manager.games[self.id]["host"]:
+                werewolf_manager.games[self.id]["players"].add(interaction.user.id)
+            else:
+                await interaction.response.send_message(
+                    "募集者は参加できません", ephemeral=True
+                )
+                return
+
+            await update_recruiting_embed(self.id, interaction)
+        else:
+            await interaction.response.send_message(
+                "すでに参加しています", ephemeral=True
+            )
+
+    @discord.ui.button(label="退出", style=discord.ButtonStyle.red)
+    async def leave(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user.id in werewolf_manager.games[self.id]["players"]:
+            if interaction.user.id != werewolf_manager.games[self.id]["host"]:
+                werewolf_manager.games[self.id]["players"].remove(interaction.user.id)
+
+            await update_recruiting_embed(self.id, interaction)
+        elif interaction.user.id == werewolf_manager.games[self.id]["host"]:
+            await interaction.response.send_message(
+                "募集者は退出できません", ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                "あなたは参加していません", ephemeral=True
+            )
+
+    @discord.ui.button(label="開始", style=discord.ButtonStyle.primary)
+    async def start(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user.id == werewolf_manager.games[self.id]["host"]:
+            pass
+        else:
+            await interaction.response.send_message(
+                "あなたは募集者ではありません", ephemeral=True
+            )
+
+    @discord.ui.button(label="中止", style=discord.ButtonStyle.grey)
+    async def end(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user.id == werewolf_manager.games[self.id]["host"]:
+            embed = discord.Embed(
+                title=f"人狼ゲーム",
+                description="募集が中止されました",
+                color=discord.Color.red(),
+            )
+
+            await interaction.response.edit_message(embed=embed, view=None)
+            del werewolf_manager.games[self.id]
+        else:
+            await interaction.response.send_message(
+                "あなたは募集者ではありません", ephemeral=True
+            )
+
+    """
+    @discord.ui.button(label="⚙️", style=discord.ButtonStyle.grey)
+    async def setting(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+    """
+
+
+class WerewolfManager:
+    def __init__(self):
+        self.games = {}
+
+    def create_game(
+        self,
+        game_id: int,
+        host_id: int,
+        limit: int,
+        message_id: int,
+        channel_id: int,
+        view: JoinView,
+    ) -> int:
+        self.games[game_id] = {
+            "host": host_id,
+            "players": set(),
+            "roles": {role.Werewolf(): 1},
+            "limit": limit,
+            "message_id": message_id,
+            "channel_id": channel_id,
+            "view": view,
+        }
+
+        return game_id
+
+    def delete_game(self, game_id: int):
+        del self.games[game_id]
+
+
+werewolf_manager = WerewolfManager()
+
+
+async def update_recruiting_embed(
+    game_id: int, interaction: discord.Interaction | None = None
+) -> discord.Embed:
+    game_info = werewolf_manager.games[game_id]
+
     embed = discord.Embed(
         title=f"人狼ゲーム({len(game_info['players']) + 1}/{game_info['limit']}人)",
         description=f"募集者:<@!{game_info['host']}>",
@@ -38,89 +154,12 @@ def make_participants_embed(game_info: dict) -> discord.Embed:
         or "なし",
         inline=False,
     )
-    return embed
-
-
-all_game_info = {}
-
-
-class JoinView(discord.ui.View):
-    def __init__(self, id, timeout=180, limit=10):
-        super().__init__(timeout=timeout)
-        self.id = id
-        self.limit = limit
-
-    @discord.ui.button(label="参加", style=discord.ButtonStyle.success)
-    async def join(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if interaction.user.id not in all_game_info[self.id]["players"]:
-            if len(all_game_info[self.id]["players"]) + 1 >= self.limit:
-                await interaction.response.send_message(
-                    "人数制限に達しました", ephemeral=True
-                )
-                return
-
-            if interaction.user.id != all_game_info[self.id]["host"]:
-                all_game_info[self.id]["players"].add(interaction.user.id)
-
-            await interaction.response.edit_message(
-                embed=make_participants_embed(all_game_info[self.id]),
-                view=self,
-            )
-        else:
-            await interaction.response.send_message(
-                "すでに参加しています", ephemeral=True
-            )
-
-    @discord.ui.button(label="退出", style=discord.ButtonStyle.red)
-    async def leave(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if interaction.user.id in all_game_info[self.id]["players"]:
-            if interaction.user.id != all_game_info[self.id]["host"]:
-                all_game_info[self.id]["players"].remove(interaction.user.id)
-
-            await interaction.response.edit_message(
-                embed=make_participants_embed(all_game_info[self.id]),
-                view=self,
-            )
-        elif interaction.user.id == all_game_info[self.id]["host"]:
-            await interaction.response.send_message(
-                "募集者は退出できません", ephemeral=True
-            )
-        else:
-            await interaction.response.send_message(
-                "あなたは参加していません", ephemeral=True
-            )
-
-    @discord.ui.button(label="開始", style=discord.ButtonStyle.primary)
-    async def start(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if interaction.user.id == all_game_info[self.id]["host"]:
-            pass
-        else:
-            await interaction.response.send_message(
-                "あなたは募集者ではありません", ephemeral=True
-            )
-
-    @discord.ui.button(label="中止", style=discord.ButtonStyle.grey)
-    async def end(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if interaction.user.id == all_game_info[self.id]["host"]:
-            embed = discord.Embed(
-                title=f"人狼ゲーム",
-                description="募集が中止されました",
-                color=discord.Color.red(),
-            )
-
-            await interaction.response.edit_message(embed=embed, view=None)
-            del all_game_info[self.id]
-        else:
-            await interaction.response.send_message(
-                "あなたは募集者ではありません", ephemeral=True
-            )
-
-    """
-    @discord.ui.button(label="⚙️", style=discord.ButtonStyle.grey)
-    async def setting(
-        self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-    """
+    if interaction:
+        await interaction.response.edit_message(embed=embed, view=game_info["view"])
+    else:
+        channel = client.get_channel(game_info["channel_id"])
+        message = await channel.fetch_message(game_info["message_id"])
+        await message.edit(embed=embed, view=game_info["view"])
 
 
 @tree.command(name="werewolf", description="人狼ゲームをプレイします")
@@ -129,24 +168,21 @@ class JoinView(discord.ui.View):
     limit=[discord.app_commands.Choice(name=i, value=i) for i in range(3, 16)]
 )
 async def werewolf(interaction: discord.Interaction, limit: int = 10):
-    view = JoinView(id=interaction.id, timeout=None, limit=limit)
-
-    game_info = {
-        "host": interaction.user.id,
-        "players": set(),
-        "roles": {role.Werewolf(): 1},
-        "limit": limit,
-    }
-    
+    view = JoinView(id=interaction.id, timeout=None)
 
     await interaction.response.defer()
-    message = await interaction.followup.send(
-        embed=make_participants_embed(game_info), view=view
-    )
-    
-    game_info["message_id"] = message.id
-    all_game_info[interaction.id] = game_info
+    message = await interaction.followup.send(view=view)
 
+    werewolf_manager.create_game(
+        game_id=interaction.id,
+        host_id=interaction.user.id,
+        limit=limit,
+        message_id=message.id,
+        channel_id=interaction.channel_id,
+        view=view,
+    )
+
+    await update_recruiting_embed(interaction.id)
 
 
 @tree.command(name="role", description="人狼ゲームの役職を設定します")
@@ -161,27 +197,17 @@ async def werewolf(interaction: discord.Interaction, limit: int = 10):
 async def set_role(interaction: discord.Interaction, role_name: str, number: int):
     host_game_id = [
         game_id
-        for game_id, game_info in all_game_info.items()
+        for game_id, game_info in werewolf_manager.games.items()
         if game_info["host"] == interaction.user.id
     ][0]
 
     if host_game_id:
-        all_game_info[host_game_id]["roles"][role.roles[role_name]] = number
+        werewolf_manager.games[host_game_id]["roles"][role.roles[role_name]] = number
         await interaction.response.send_message(
             f"{role_name}を{number}人に設定しました", ephemeral=True
         )
 
-        view = JoinView(
-            id=host_game_id, timeout=None, limit=all_game_info[host_game_id]["limit"]
-        )
-        channel = client.get_channel(interaction.channel_id)
-        message = await channel.fetch_message(all_game_info[host_game_id]["message_id"])
-        await message.edit(
-            embed=make_participants_embed(
-                all_game_info[host_game_id],
-            ),
-            view=view
-        )
+        await update_recruiting_embed(host_game_id)
     else:
         await interaction.response.send_message("ゲームが存在しません", ephemeral=True)
 
